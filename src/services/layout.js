833beb2aa0c73a1cf47383c016d814e6168a28bb
Angie the Fairytale <konzeptmeister@gmail.com>
2019-08-18 11:46:45 +0200

    refactor layout service

--- src/services/layout.js
+++ src/services/layout.js
@@ -2,59 +2,71 @@ import BaseService from "./base-service.js";
 import { observable, action, decorate } from "mobx";
 import { STATUS } from "../utils.js";
 
-const PROFILES = {
-  SMALL: {
-    description: "small layout profile - a phone usually",
-    min_width: 1,
-    min_height: 1
+const PROFILES = [
+  {
+    id: "LARGE",
+    min_width: 900,
+    min_height: 520
   },
-  MEDIUM: {
-    description: "medium layout profile - a tablet usually",
+  {
+    id: "MEDIUM",
     min_width: 600,
     min_height: 400
   },
-  LARGE: {
-    description: "large layout profile - a desktop usually",
-    min_width: 900,
-    min_height: 520
+  {
+    id: "SMALL",
+    min_width: 1,
+    min_height: 1
   }
-};
+];
 
 class LayoutService extends BaseService {
 
   setup(){
     this.name = "layout";
-    this.status = STATUS.READY;
+    this.status = STATUS.INIT;
     this.width = 1;
     this.height = 1;
     this.profile = "SMALL";
-    this.resizeHandler();
-    window.addEventListener("resize", this.resizeHandler.bind(this));
+    this.projector = false;
+    this.element = d3.select(this.model.placeholder || "body").node();
+    this._resizeHandler();
+    window.addEventListener("resize", this._resizeHandler.bind(this));
   }
 
-  resizeHandler(){
+  _resizeHandler(){
     action(()=>{
-      const view = d3.select(this.model.placeholder || "body").node();
-      this.width = view.clientWidth;
-      this.height = view.clientHeight;
-      this.profile = this.detectProfile(this.width, this.height);
-      if (!this.profile) console.warn(`
-        Placeholder ${this.model.placeholder || "body"} is too little: ${this.width} x ${this.height} px, 
-        nothing should be rendered
-      `);
+      this.width = this.element.clientWidth;
+      this.height = this.element.clientHeight;
+      
+      const profile = PROFILES.find(p => (this.width >= p.min_width && this.height >= p.min_height));
+
+      if (!profile) {
+        this.profile = null;
+        this.status = STATUS.ERROR;
+        console.warn(`
+          Layout service: nothing should be rendered, because
+          placeholder ${this.model.placeholder || "body"} has display:none or is too little: ${this.width} x ${this.height} px
+        `);
+      } else {
+        this.profile = profile.id;
+        this.status = STATUS.READY;
+      }
     })();
   }
-  
-  detectProfile(w, h) {
-    let profile = null;
-    Object.keys(PROFILES).forEach(p => {
-      if (h >= PROFILES[p].min_height && w >= PROFILES[p].min_width) profile = p;
-    });
-    return profile;
+
+  getProfileConstants(normalConstants = {}, forProjector = {}){
+    if (!this.projector) 
+      return normalConstants[this.profile] || {};
+    else
+      return Object.assign({}, normalConstants[this.profile] || {}, forProjector[this.profile] || {});
   }
 }
 
 export default decorate(LayoutService, {
+  "projector": observable,
+  "width": observable,
+  "height": observable,
   "profile": observable,
   "status": observable
 });
