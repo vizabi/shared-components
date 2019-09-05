184a3d03706317cf3b1c38199603f6c1fb3cc183
Dmitry Basov <dmbasov@gmail.com>
2019-09-05 19:43:59 +0700

    layout service set name,orientation,size classes on placeholder

--- src/tools/barrankchart.js
+++ src/tools/barrankchart.js
@@ -11,7 +11,7 @@ export default class Barrankchart extends BaseComponent {
   constructor(config){
     config.subcomponents = [{
       type: VizabiBarrankchart,
-      placeholder: ".vzb-placeholder",
+      placeholder: ".vzb-barrankchart",
       //model: this.model
     },{
       type: TimeSlider,
@@ -20,7 +20,7 @@ export default class Barrankchart extends BaseComponent {
     }];
 
     config.template = `
-      <div class="vzbp-viewer vzb-placeholder" style="width:100%; height:100%;"></div>
+      <div class="vzb-barrankchart" style="width:100%; height:100%;"></div>
       <div class="vzb-timeslider"></div>
     `;
 
