a2332dcd0a193bda0972fcd757f151a9228f14b3
Dmitry Basov <dmbasov@gmail.com>
2019-11-29 00:48:46 +0300

    add collision resolver

--- src/index.js
+++ src/index.js
@@ -13,8 +13,10 @@ import * as _Icons from "./icons/iconset";
 import * as _Utils from "./utils";
 import * as _LegacyUtils from "./legacy/base/utils";
 import _axisSmart from "./legacy/helpers/d3.axisWithLabelPicker";
+import _collisionResolver from "./legacy/helpers/d3.collisionResolver";
 
 export const Icons = _Icons;
 export const Utils = _Utils;
 export const LegacyUtils = _LegacyUtils;
 export const axisSmart = _axisSmart;
+export const collisionResolver = _collisionResolver;
