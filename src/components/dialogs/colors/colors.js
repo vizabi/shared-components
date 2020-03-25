7978c3fc188572ead2248aff1acb1f87a4462877
Dmitry Basov <dmbasov@gmail.com>
2020-03-25 22:47:24 +0300

    fix color dialog

--- src/components/dialogs/colors/colors.js
+++ src/components/dialogs/colors/colors.js
@@ -1,15 +1,53 @@
 import * as utils from "base/utils";
-import Component from "base/component";
 import { Dialog } from "../dialog";
-
-import colorlegend from "components/colorlegend/colorlegend";
-import indicatorpicker from "components/indicatorpicker/indicatorpicker";
+import { IndicatorPicker } from "../../indicatorpicker/indicatorpicker";
 
 /*!
  * VIZABI COLOR DIALOG
  */
 
 export class Colors extends Dialog {
+  constructor(config) {
+    config.template = `
+      <div class='vzb-dialog-modal'>
+        <span class="thumb-tack-class thumb-tack-class-ico-pin fa" data-dialogtype="colors" data-click="pinDialog"></span>
+        <span class="thumb-tack-class thumb-tack-class-ico-drag fa" data-dialogtype="colors" data-click="dragDialog"></span>
+        
+        <div class="vzb-dialog-title">
+          <span data-localise="buttons/colors"></span>
+          <span class="vzb-caxis-selector"></span>
+        </div>
+      
+        <div class="vzb-dialog-content vzb-dialog-scrollable">
+          <div class="vzb-clegend-container">
+            <svg>
+              <g class="vzb-timedisplay"></g>
+            </svg>
+          </div>
+        </div>
+
+        <div class="vzb-dialog-buttons">
+          <div data-click="closeDialog" class="vzb-dialog-button vzb-label-primary">
+            <span data-localise="buttons/ok"></span>
+          </div>
+        </div>
+
+      </div>
+    `;
+    
+    config.subcomponents = [{
+      type: IndicatorPicker,
+      placeholder: ".vzb-caxis-selector",
+      options: {
+        submodel: "encoding",
+        targetProp: "color",
+        showHoverValues: true
+      },
+      //model: config.root.model.stores.markers.get("legend")
+    }]
+    
+    super(config);
+  }
 
 }
 
