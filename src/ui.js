7dc493342660f3bacaaef786ed9d81ab2aee64c7
Dmitry Basov <dmbasov@gmail.com>
2020-06-15 03:36:17 +0300

    fix ui state tree creation

--- src/ui.js
+++ src/ui.js
@@ -1,26 +1,55 @@
-import { observable } from "mobx";
+import { observable, extendObservable } from "mobx";
 import * as utils from "./legacy/base/utils";
 
-export function ui(defaults = {}, config, parentConfig = {}) {
+export function ui(defaults = {}, config = {}, baseConfig = {}) {
     const _ui = {};
-    const defaultKeys = Object.keys(defaults);
-    const configKeys = Object.keys(config);
-    const parentConfigKeys = Object.keys(config);
+    const defaultKeys = Object.getOwnPropertyNames(defaults);
+    const configKeys = Object.getOwnPropertyNames(config);
+    const baseConfigKeys = Object.getOwnPropertyNames(baseConfig);
 
-    for (let key of new Set([...defaultKeys, ...configKeys, ...parentConfigKeys])) {
-        const descriptor = {
-            get() {
-                if (utils.isPlainObject(config[key]) || utils.isPlainObject(config[key])) {
-                    return ui(defaults[key], config[key] || (config[key] = {}), parentConfig[key]);
-                }
-                return (typeof config[key] !== "undefined") ? config[key] : 
-                    (typeof parentConfig[key] !== "undefined") ? parentConfig[key] : defaults[key];
-            },
-            set(value) { config[key] = value },
-            enumerable: true,
-            configurable: true
-        };
-        Object.defineProperty(_ui, key, descriptor);
+    for (let key of new Set([...defaultKeys, ...configKeys, ...baseConfigKeys])) {
+        if (typeof defaults[key] !== "function" && (utils.isPlainObject(config[key]) || utils.isPlainObject(baseConfig[key]) || utils.isPlainObject(defaults[key]))) {
+            if (!config[key]) extendObservable(config, {[key] : {}});
+                
+            _ui[key] = ui(defaults[key], config[key], baseConfig[key]);
+        } else {
+                const newObj = {};
+                const descriptor = {
+                    get() {
+                        return (typeof config[key] !== "undefined") ? config[key] : 
+                            (typeof baseConfig[key] !== "undefined") ? baseConfig[key] : 
+                            typeof defaults[key] === "function" ? utils.deepClone(defaults[key]()) : defaults[key];
+                    },
+                    set(value) { config[key] = value },
+                    enumerable: true,
+                    configurable: true
+                };
+                Object.defineProperty(_ui, key, descriptor);
+        }
     }
     return observable(_ui);
 }
+
+// export function ui2(defaults = {}, config, parentConfig = {}) {
+//     const _ui = {};
+//     const defaultKeys = Object.keys(defaults);
+//     const configKeys = Object.keys(config);
+//     const parentConfigKeys = Object.keys(config);
+
+//     for (let key of new Set([...defaultKeys, ...configKeys, ...parentConfigKeys])) {
+//         const descriptor = {
+//             get() {
+//                 if (utils.isPlainObject(config[key]) || utils.isPlainObject(config[key])) {
+//                     return ui2(defaults[key], config[key] || (config[key] = {}), parentConfig[key]);
+//                 }
+//                 return (typeof config[key] !== "undefined") ? config[key] : 
+//                     (typeof parentConfig[key] !== "undefined") ? parentConfig[key] : defaults[key];
+//             },
+//             set(value) { config[key] = value },
+//             enumerable: true,
+//             configurable: true
+//         };
+//         Object.defineProperty(_ui, key, descriptor);
+//     }
+//     return observable(_ui);
+// }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import { deepClone, deepExtend } from "./legacy/base/utils";
+
 export const STATUS = {
   INIT: "init", 
   PENDING: "pending", 
@@ -16,5 +18,36 @@ export function getConceptProps(model, localise) {
       });
   } else {
       return model.data.conceptProps;
+  }
+}
+
+export function getDefaultStateTree(defaultState, component) {
+  const _defaultState = getChildrenDefaultState(defaultState, component.DEFAULT_UI);
+  component.children.forEach(child => {
+    if (child.name) {
+      _defaultState[child.name] = getDefaultStateTree(_defaultState[child.name], child);
+    } else {
+      deepExtend(_defaultState, getDefaultStateTree(_defaultState, child));
+    }
+  });
+  return _defaultState;
+}
+
+export function getChildrenDefaultState(parent, children) {
+  const cloneChildren = deepClone(children);
+  return deepExtend(cloneChildren, parent);
+}
+
+export function clearEmpties(obj) {
+  for (const key in obj) {
+    if (!obj[key] || typeof obj[key] !== "object") {
+      continue // If null or not an object, skip to the next iteration
+    }
+
+    // The property is an object
+    clearEmpties(obj[key]); // <-- Make a recursive call on the nested object
+    if (Object.keys(obj[key]).length === 0) {
+      delete obj[key]; // The object had no properties, so delete that property
+    }
   }
 }
