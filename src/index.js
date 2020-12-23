0245e8b92e4033f607c2c6dca450cb8993fad2be
Dmitry Basov <dmbasov@gmail.com>
2020-12-23 23:56:41 +0300

    add textellipsis and tap,longtap helpers

--- src/index.js
+++ src/index.js
@@ -14,9 +14,15 @@ import * as _Utils from "./utils";
 import * as _LegacyUtils from "./legacy/base/utils";
 import _axisSmart from "./legacy/helpers/d3.axisWithLabelPicker";
 import _collisionResolver from "./legacy/helpers/d3.collisionResolver";
+import _TextEllipsis from "./legacy/helpers/textEllipsis";
+
+import { onLongTap, onTap } from "./legacy/helpers/d3.touchEvents";
+d3.selection.prototype.onTap = onTap;
+d3.selection.prototype.onLongTap = onLongTap;
 
 export const Icons = _Icons;
 export const Utils = _Utils;
 export const LegacyUtils = _LegacyUtils;
 export const axisSmart = _axisSmart;
 export const collisionResolver = _collisionResolver;
+export const TextEllipsis = _TextEllipsis;
