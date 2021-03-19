46b0fc58d029701c1864528d0927c2bbc145f22c
Dmitry Basov <dmbasov@gmail.com>
2021-03-19 04:28:04 +0300

    add Chart abstract collection for chart components of tools

---
+++ src/components/chart/chart.js
@@ -0,0 +1,13 @@
+import { BaseComponent } from "../base-component";
+
+const CollectionMixin = superClass => class extends superClass {
+  //static _collection = {};
+  static add(name, addedClass) {
+      CollectionMixin._collection[name] = addedClass;
+  }
+  static get(name) { return CollectionMixin._collection[name]}
+}
+
+CollectionMixin._collection = {};
+
+export class Chart extends CollectionMixin(BaseComponent) {}
