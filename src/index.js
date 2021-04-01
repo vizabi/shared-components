bfc19ff0640d9c57c981a85e483dbf91ee42657b
Angie <konzeptmeister@gmail.com>
2021-04-01 16:47:54 +0200

    v1.6.7: Don't need vizabi-reactive in shared components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-// import Vizabi from "vizabi-reactive";
-// import BarrankChart from "./tools/barrankchart.js";
-// import LocaleService from "./services/locale.js";
-// import LayoutService from "./services/layout.js";
-
-// window.Vizabi = Vizabi;
-// window.BarrankChart = BarrankChart;
-// window.LocaleService = LocaleService;
-// window.LayoutService = LayoutService;
-
-
 import * as _Icons from "./icons/iconset";
 import * as _Utils from "./utils";
 import * as _LegacyUtils from "./legacy/base/utils";
