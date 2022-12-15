603c8501a1b4dcd84b7a81585540746b055869d6
angie <konzeptmeister@gmail.com>
2022-12-15 12:18:54 +0100

    v1.27.0: Add long number formatter

--- src/services/locale.js
+++ src/services/locale.js
@@ -130,6 +130,12 @@ class _LocaleService extends BaseService {
       return (formatted + suffix + (options === PERCENT || options === SHARE ? "%" : ""));
     };
 
+    this.longNumberF = d3.formatLocale({
+      decimal: ".",
+      thousands: " ", //short space
+      grouping: [3],
+    }).format(',.2r');
+
     this.dateF = {
       year: d3.timeFormat("%Y"),
       month: d3.timeFormat("%Y-%m"),
